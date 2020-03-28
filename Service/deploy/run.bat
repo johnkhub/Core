789598ab06b509649aea7387a20c52b4d8bfa860
frank <frank.van.riet@imqs.co.za>
2020-03-28 20:33:52 +0200

    Run scripts

--- Service/deploy/run.bat
+++ Service/deploy/run.bat
@@ -1,2 +1,1 @@
-C:\Users\frankvr\Downloads\openjdk-12.0.1_windows-x64_bin\jdk-12.0.1\bin\java -Dlog_name=imqs-input-form.log -Dlogging.config=logback.groovy -Dspring.profiles.active=production -jar ../target/imqs-core-service.jar file:config.json --server.port=8669
-
+C:\Users\frankvr\Downloads\openjdk-12.0.1_windows-x64_bin\jdk-12.0.1\bin\java -Dlogging.config=logback.groovy -Dspring.profiles.active=production -jar asset-core-service.jar file:config.json --server.port=8669
---
+++ Service/deploy/run.sh
@@ -0,0 +1,2 @@
+java -Dlogging.config=logback.groovy -Dspring.profiles.active=production -jar asset-core-service.jar file:config.json --server.port=8669
+
