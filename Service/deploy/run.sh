8d7e4c08f134504d6d8b0e9599c1acda4c831e4f
frank <frank.van.riet@imqs.co.za>
2020-04-07 20:05:54 +0200

    Deleted dead stuff

--- Service/deploy/run.sh
+++ Service/deploy/run.sh
@@ -1,2 +1,2 @@
-java -Dlogging.config=logback.groovy -Dspring.profiles.active=production -jar asset-core-service.jar file:config.json --server.port=8669
+java -Dlogging.config=logback.groovy -Dspring.profiles.active=production -jar asset-core-service.jar --server.port=8669 --config=file:config.json
 
