af95c1159967a0f357c014c35ecaa84925d783a1
frank <frank.van.riet@imqs.co.za>
2020-04-08 14:32:29 +0200

    Update .gitignore, ref_branch_202003282055.sql, and 2 more files...

---
+++ Service/deploy/ref_branch_202003282055.sql
@@ -0,0 +1,4 @@
+INSERT INTO dtpw.ref_branch (k,v,creation_date,deactivated_at,allow_delete,activated_at) VALUES 
+('B_PPW',' Provincial Public Works','2020-03-28 20:26:05.004',NULL,false,'2020-03-28 20:26:05.004')
+,('B_RNM',' Road Network Management','2020-03-28 20:26:05.004',NULL,false,'2020-03-28 20:26:05.004')
+;
