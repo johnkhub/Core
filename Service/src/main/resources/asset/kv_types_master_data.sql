63ee25e6b7a9ff18a191690b9fd48e90d24da88a
frank <frank.van.riet@imqs.co.za>
2020-05-07 18:21:32 +0200

    Fix missed kv type

--- Service/src/main/resources/asset/kv_types_master_data.sql
+++ Service/src/main/resources/asset/kv_types_master_data.sql
@@ -4,3 +4,4 @@ INSERT INTO kv_type (code,name,"table") VALUES ('TOWN', 'Town', 'asset.ref_town'
 INSERT INTO kv_type (code,name,"table") VALUES ('MUNIC', 'Municiplaity', 'asset.ref_municipality');
 INSERT INTO kv_type (code,name,"table") VALUES ('DISTRICT', 'District', 'asset.ref_district');
 INSERT INTO kv_type (code,name,"table") VALUES ('SUBURB', 'Suburb', 'asset.ref_suburb');
+INSERT INTO kv_type (code,name,"table") VALUES ('FACIL_TYPE', 'Facility Type', 'asset.ref_facility_type');
