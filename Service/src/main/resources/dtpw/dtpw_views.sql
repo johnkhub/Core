4241cad3063fb608145d7361526a753797e2b37a
frank <frank.van.riet@imqs.co.za>
2020-06-29 12:14:16 +0200

    schema sync

--- Service/src/main/resources/dtpw/dtpw_views.sql
+++ Service/src/main/resources/dtpw/dtpw_views.sql
@@ -2,7 +2,7 @@ DROP VIEW IF EXISTS dtpw.asset_core_dtpw_view CASCADE;
 
 CREATE OR REPLACE VIEW dtpw.asset_core_dtpw_view
 AS SELECT
-       '1_1' AS obj_version,
+       '1_2' AS obj_version,
        core.asset_id,
        core.asset_type_code,
        core.name,
@@ -31,7 +31,6 @@ AS SELECT
             LEFT JOIN asset.a_tp_envelope a_tp_e ON e.asset_id = a_tp_e.asset_id
             LEFT JOIN asset f ON subpath(core.func_loc_path, 0, 2) = f.func_loc_path
             LEFT JOIN asset.a_tp_facility a_tp_f ON f.asset_id = a_tp_f.asset_id
-       /*asset.ref_ joins added*/
             LEFT JOIN asset.ref_district rd ON rd.k = a_tp_e.district_code
             LEFT JOIN asset.ref_municipality rm ON rm.k = a_tp_e.municipality_code
             LEFT JOIN asset.ref_town rt ON rt.k = a_tp_e.town_code
@@ -53,28 +52,38 @@ FROM
 COMMENT ON VIEW dtpw.asset_core_dtpw_view_with_lpi IS 'Adds lpi to asset_core_dtpw_view ';
 
 
-CREATE MATERIALIZED VIEW dtpw.dtpw_core_report_view AS
-    SELECT asset_core_dtpw_view.asset_id,
-          asset_core_dtpw_view.asset_type_code,
-          asset_core_dtpw_view.name,
-          asset_core_dtpw_view.func_loc_path::text,
-          asset_core_dtpw_view.active,
-          asset_core_dtpw_view.latitude,
-          asset_core_dtpw_view.longitude,
-          asset_core_dtpw_view.address,
-          asset_core_dtpw_view.barcode,
-          asset_core_dtpw_view.serial_number,
-          asset_core_dtpw_view.district_code,
-          asset_core_dtpw_view.municipality_code,
-          asset_core_dtpw_view.town_code,
-          asset_core_dtpw_view.suburb_code,
-          asset_core_dtpw_view.facility_type_code,
-          asset_core_dtpw_view.responsible_dept_code,
-          asset_core_dtpw_view.is_owned,
-          asset_core_dtpw_view."EMIS",
-          asset_core_dtpw_view.geom
-   FROM dtpw.asset_core_dtpw_view
+CREATE MATERIALIZED VIEW dtpw.dtpw_core_report_view
+AS
+SELECT '1_2'::text AS obj_version,
+       asset_core_dtpw_view.asset_id,
+       asset_core_dtpw_view.asset_type_code,
+       asset_core_dtpw_view.name,
+       asset_core_dtpw_view.func_loc_path::text AS func_loc_path,
+       asset_core_dtpw_view.active,
+       asset_core_dtpw_view.latitude,
+       asset_core_dtpw_view.longitude,
+       asset_core_dtpw_view.address,
+       asset_core_dtpw_view.geom,
+       asset_core_dtpw_view.barcode,
+       asset_core_dtpw_view.serial_number,
+       asset_core_dtpw_view.district_code,
+       asset_core_dtpw_view.district_value,
+       asset_core_dtpw_view.municipality_code,
+       asset_core_dtpw_view.municipality_value,
+       asset_core_dtpw_view.town_code,
+       asset_core_dtpw_view.town_value,
+       asset_core_dtpw_view.suburb_code,
+       asset_core_dtpw_view.suburb_value,
+       asset_core_dtpw_view.facility_type_code,
+       asset_core_dtpw_view.responsible_dept_code,
+       asset_core_dtpw_view.is_owned,
+       asset_core_dtpw_view."EMIS"
+FROM dtpw.asset_core_dtpw_view
 WITH DATA;
+
+COMMENT ON MATERIALIZED VIEW dtpw.dtpw_core_report_view
+    IS 'This an example of a materialized view that flattens out the information in the core. It should be useful as the basis for many reports.';
+
 
 
 CREATE UNIQUE INDEX m1_asset_id_idx ON dtpw.dtpw_core_report_view USING btree (asset_id);
@@ -117,3 +126,38 @@ SELECT
 FROM dtpw.asset_core_dtpw_view;
 
 COMMENT ON VIEW dtpw.dtpw_export_view IS 'This is an exact copy of the select in dtpw.dtpw_core_report_view but it converts the geometry to well-known text.';
+
+
+-- View: dtpw.dtpw_core_report_view_wrapper
+
+-- DROP VIEW dtpw.dtpw_core_report_view_wrapper;
+
+CREATE OR REPLACE VIEW dtpw.dtpw_core_report_view_wrapper
+AS
+SELECT dtpw_core_report_view.obj_version,
+       dtpw_core_report_view.asset_id,
+       dtpw_core_report_view.asset_type_code,
+       dtpw_core_report_view.name,
+       dtpw_core_report_view.func_loc_path,
+       dtpw_core_report_view.active,
+       dtpw_core_report_view.latitude,
+       dtpw_core_report_view.longitude,
+       dtpw_core_report_view.address,
+       dtpw_core_report_view.geom,
+       dtpw_core_report_view.barcode,
+       dtpw_core_report_view.serial_number,
+       dtpw_core_report_view.district_code,
+       dtpw_core_report_view.district_value,
+       dtpw_core_report_view.municipality_code,
+       dtpw_core_report_view.municipality_value,
+       dtpw_core_report_view.town_code,
+       dtpw_core_report_view.town_value,
+       dtpw_core_report_view.suburb_code,
+       dtpw_core_report_view.suburb_value,
+       dtpw_core_report_view.facility_type_code,
+       dtpw_core_report_view.responsible_dept_code,
+       dtpw_core_report_view.is_owned,
+       dtpw_core_report_view."EMIS"
+FROM dtpw.dtpw_core_report_view;
+
+--GRANT SELECT ON TABLE dtpw.dtpw_core_report_view_wrapper TO powerbi;
