3fd222779a9d4451d9e1ed61eaa1207bbba28ae3
frank <frank.van.riet@imqs.co.za>
2020-07-09 14:27:46 +0200

    Rework constraints

--- Service/src/main/resources/public/checkconstraints.sql
+++ Service/src/main/resources/public/checkconstraints.sql
@@ -61,4 +61,55 @@ COMMENT ON FUNCTION public.fn_check_valid_func_loc_path IS 'Variation of fn_is_v
 
 ALTER TABLE public.asset ADD CONSTRAINT asset_check_func_loc_path CHECK ((public.fn_check_valid_func_loc_path(func_loc_path) = true));
 
+
+CREATE OR REPLACE FUNCTION public.fn_identify_multiple_subclasses(the_asset uuid) RETURNS boolean AS $$
+DECLARE
+    sub_classes  text[];
+    stmt text;
+    num int;
+    total int;
+    idx int;
+BEGIN
+    -- During a restore, data is inserted before indexes are created. Since the constraint checks against
+    -- data in the system checking this constraint is catastrophically slow and ultimately useless as it
+    -- as we are restoring a database that is already valid.
+    idx := (
+        SELECT
+            count(t.relname) as table_name
+        FROM
+            pg_class t,  pg_class i,  pg_index ix,  pg_attribute a
+        WHERE
+                t.oid = ix.indrelid
+          AND i.oid = ix.indexrelid
+          AND a.attrelid = t.oid
+          AND a.attnum = ANY(ix.indkey)
+          AND t.relkind = 'r'
+          AND t.relname = 'asset'
+    );
+
+    IF idx = 0 THEN
+        RETURN true;
+    END IF;
+
+    num := 0;
+    total := 0;
+    sub_classes := 	ARRAY(SELECT code FROM public.assettype);
+    FOR clss IN 1..coalesce(array_upper(sub_classes,1),0)
+        LOOP
+            stmt := format('SELECT count(asset_id) FROM asset.a_tp_%s WHERE asset_id=''%s''::UUID', sub_classes[clss], the_asset);
+            EXECUTE stmt into num;
+            total := total + num;
+        END LOOP;
+
+    --raise notice 'Number %', total;
+    RETURN total > 1;
+END ; $$
+    LANGUAGE PLPGSQL
+    SECURITY DEFINER
+;
+//
+COMMENT ON FUNCTION public.fn_identify_multiple_subclasses IS 'Identifies if the same asset has multiple subclasses e.g. same uuid in say envelope and facility. This is an invalid state but at the moment there is nothing in the database constraints that stops you from doing this.';
+
+
+
 ALTER TABLE public.asset ADD CONSTRAINT asset_subclass CHECK (public.fn_identify_multiple_subclasses(asset_id) = false);
