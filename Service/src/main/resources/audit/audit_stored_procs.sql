04a9b4bad6901619b446f3031b5962c00880d9cc
frank <frank.van.riet@imqs.co.za>
2020-11-09 15:16:42 +0200

    Make use of redefinition of function defined in public schema to get around circular dependency to audit schema

--- Service/src/main/resources/audit/audit_stored_procs.sql
+++ Service/src/main/resources/audit/audit_stored_procs.sql
@@ -47,3 +47,21 @@ END; $$
     SECURITY DEFINER
 ;
 //
+
+
+-- We REDEFINE this function as we have a circular dependency.  fn_delete_asset needs to be able to log when it executes
+-- but at the time the function is created the asset_schema may not yet exist
+CREATE OR REPLACE FUNCTION public.log_audit(
+    principal_id uuid,
+    event_time timestamp without time zone,
+    action text,
+    status text,
+    parameters jsonb,
+    entity_id uuid
+) RETURNS void AS $$
+BEGIN
+    PERFORM audit.log( principal_id,event_time,action,status,parameters,entity_id);
+END; $$
+    LANGUAGE PLPGSQL
+    SECURITY DEFINER
+;
