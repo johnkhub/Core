99521d7ad1c4ee73cd99c58a13ec41278075662d
frank <frank.van.riet@imqs.co.za>
2020-12-17 09:22:37 +0200

    Update fix

--- Service/manual/schema-update.sql
+++ Service/manual/schema-update.sql
@@ -339,7 +339,7 @@ CREATE TABLE public.ref_performance_rating
     deactivated_at timestamp without time zone,
     allow_delete boolean DEFAULT false,
     activated_at timestamp without time zone NOT NULL DEFAULT now(),
-    condition_standard text COLLATE pg_catalog."default" NOT NULL
+    asset_info text COLLATE pg_catalog."default" NOT NULL
 )
     WITH (
         OIDS = FALSE
