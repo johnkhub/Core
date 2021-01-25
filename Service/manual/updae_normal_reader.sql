481538cd1dbb081ac383f2cf87d64b64069feca4
frank <frank.van.riet@imqs.co.za>
2021-01-25 10:08:29 +0200

    Data fixes

---
+++ Service/manual/updae_normal_reader.sql
@@ -0,0 +1,15 @@
+GRANT SELECT ON TABLE public.ref_accessibility_rating TO normal_reader;
+GRANT SELECT ON TABLE public.ref_asset_class TO normal_reader;
+GRANT SELECT ON TABLE public.ref_asset_nature TO normal_reader;
+GRANT SELECT ON TABLE public.ref_condition_rating TO normal_reader;
+GRANT SELECT ON TABLE public.ref_confidence_rating TO normal_reader;
+GRANT SELECT ON TABLE public.ref_criticality_rating TO normal_reader;
+GRANT SELECT ON TABLE public.ref_performance_rating TO normal_reader;
+GRANT SELECT ON TABLE public.ref_utilisation_rating TO normal_reader;
+
+GRANT SELECT ON TABLE asset.ref_facility_type TO normal_reader;
+
+GRANT SELECT ON TABLE dtpw.ref_accommodation_type TO normal_reader;
+GRANT SELECT ON TABLE dtpw.ref_deed_office TO normal_reader;
+GRANT SELECT ON TABLE dtpw.ref_land_use_class TO normal_reader;
+GRANT SELECT ON TABLE dtpw.ref_ei_district TO normal_reader;
