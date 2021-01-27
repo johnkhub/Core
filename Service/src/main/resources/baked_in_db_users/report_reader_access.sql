1f1bf244053c6238ebe37fafdf5744dcf39685d0
frank <frank.van.riet@imqs.co.za>
2021-01-27 12:42:06 +0200

    First sanity checks added

--- Service/src/main/resources/baked_in_db_users/report_reader_access.sql
+++ Service/src/main/resources/baked_in_db_users/report_reader_access.sql
@@ -46,4 +46,28 @@ GRANT USAGE ON SCHEMA access_control TO report_reader;
 GRANT EXECUTE ON FUNCTION access_control.fn_get_effective_access TO report_reader;
 GRANT EXECUTE ON FUNCTION access_control.fn_get_effective_grant TO report_reader;
 
+
+GRANT SELECT ON TABLE dtpw.ref_accommodation_type TO normal_reader;
+GRANT SELECT ON TABLE public.ref_district TO normal_reader;
+GRANT SELECT ON TABLE public.ref_town TO normal_reader;
+GRANT SELECT ON TABLE dtpw.ref_land_use_class TO normal_reader;
+GRANT SELECT ON TABLE public.ref_accessibility_rating TO normal_reader;
+GRANT SELECT ON TABLE public.ref_asset_class TO normal_reader;
+GRANT SELECT ON TABLE public.ref_region TO normal_reader;
+GRANT SELECT ON TABLE public.ref_asset_nature TO normal_reader;
+GRANT SELECT ON TABLE public.ref_ward TO normal_reader;
+GRANT SELECT ON TABLE public.ref_condition_rating TO normal_reader;
+GRANT SELECT ON TABLE public.ref_confidence_rating TO normal_reader;
+GRANT SELECT ON TABLE public.ref_criticality_rating TO normal_reader;
+GRANT SELECT ON TABLE public.ref_performance_rating TO normal_reader;
+GRANT SELECT ON TABLE public.ref_utilisation_rating TO normal_reader;
+GRANT SELECT ON TABLE dtpw.ref_deed_office TO normal_reader;
+GRANT SELECT ON TABLE asset.ref_facility_type TO normal_reader;
+GRANT SELECT ON TABLE public.ref_municipality TO normal_reader;
+GRANT SELECT ON TABLE public.ref_suburb TO normal_reader;
+GRANT SELECT ON TABLE dtpw.ref_branch TO normal_reader;
+GRANT SELECT ON TABLE dtpw.ref_chief_directorate TO normal_reader;
+GRANT SELECT ON TABLE dtpw.ref_client_department TO normal_reader;
+GRANT SELECT ON TABLE dtpw.ref_ei_district TO normal_reader;
+
 -- GRANT ROLE report_reader TO <your role here>;
